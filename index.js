6bfe41a05fb8d35068a34a5b0dfdad43594c673f
Sami Heinänen <sami.heinanen@gmail.com>
2017-12-13 12:42:48 +0200

    ...

--- index.js
+++ index.js
@@ -11,28 +11,20 @@ app.get('/', function (req, res) {
 
 app.set('port', (process.env.PORT || 5000));
 
+/*
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port: ', app.get('port'));
 });
+*/
 
 app.post('/submit-data', function (request, response){
 	
 	var xid = request.body.id
 	var xfreetext = request.body.freetext
 	// var pool = new pg.Pool();
-	response.send('you sent: ' + xid + 'foo' + xfreetext);
+	response.send('you sent: ' + xid + ':' + xfreetext);
 
-        var conString = process.env.DATABASE_URL || "postgres://pnauocceetyrto:52883e8680d07c58469cc9f2c2b5dfe6fc870aae9d85b92b05160fa77619660c@ec2-107-20-176-7.compute-1.amazonaws.com:5432/d1d6v3castj3lj";
-        var client = new pg.Client(conString);
-        client.connect();
-        var query = client.query("INSERT INTO test (id, freetext) VALUES (" + xid + ",'" + xfreetext + "')");
-        query.on("end", function (result) {
-            client.end(); 
-            response.write('Success');
-            response.end();  
-        });
-	
-/*
+
 pool.connect(function(err, client, done) {
     client.query('INSERT INTO test (id, freetext) VALUES ($1, $2)', [xid, xfreetext], (err, res) => {
 	  if (err) throw err;
@@ -42,7 +34,7 @@ pool.connect(function(err, client, done) {
     });
   done()
 pool.end()
-})*/
+})
 
 });
 
