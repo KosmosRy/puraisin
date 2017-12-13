f8d9ffcca69a415396b3cb3effe34467d7819670
Sami Heinänen <sami.heinanen@gmail.com>
2017-12-13 12:36:12 +0200

    ...

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
     res.sendFile('index.html', { root: __dirname });
 });
 
-// app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 5000));
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port: ', app.get('port'));
