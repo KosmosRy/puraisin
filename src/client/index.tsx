6bfc75e3d17e247c9d11ffc55109fc415e982218
Teppo Jalava <tjjalava@iki.fi>
2022-02-28 00:17:41 +0200

    use modules in script-tags

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -2,14 +2,7 @@ import ReactDOM from 'react-dom'
 import { AppInfo, Binge } from '../common/types'
 import App from './App'
 
-declare global {
-  interface Window {
-    __USERSTATUS__: Binge
-    __APPINFO__: AppInfo
-  }
+export default (userStatus: Binge, appInfo: AppInfo) => {
+  ReactDOM.hydrate(<App userStatus={userStatus} appInfo={appInfo} />, document.getElementById('app'))
 }
 
-const userStatus = window.__USERSTATUS__
-const appInfo = window.__APPINFO__
-
-ReactDOM.hydrate(<App userStatus={userStatus} appInfo={appInfo} />, document.getElementById('app'))
