2c25c7d59f9e2c156439b95b092a9416573a583b
Teppo Jalava <tjjalava@iki.fi>
2022-04-10 18:15:20 +0300

    Add an unregistering service-worker so that @lonzo gets his shit together

---
+++ public/service-worker.js
@@ -0,0 +1,14 @@
+/* global self */
+self.addEventListener('install', function() {
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', function() {
+  self.registration.unregister()
+    .then(function() {
+      return self.clients.matchAll()
+    })
+    .then(function(clients) {
+      clients.forEach(client => client.navigate(client.url))
+    })
+})
