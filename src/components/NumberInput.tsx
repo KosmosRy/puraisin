8830de9dc6364d240b0e7cb8602839defe76a57b
Teppo Jalava <tjjalava@iki.fi>
2023-02-25 22:03:25 +0200

    pikku stiplu

--- src/components/NumberInput.tsx
+++ src/components/NumberInput.tsx
@@ -1,53 +1,43 @@
-import { ChangeEvent, FC, InputHTMLAttributes, useState } from 'react';
+import { ChangeEvent, FC, InputHTMLAttributes, useEffect, useState } from 'react';
 
 interface NumberInputProps
   extends Omit<
     InputHTMLAttributes<HTMLInputElement>,
     'value' | 'max' | 'min' | 'step' | 'type' | 'onChange'
   > {
-  step?: number;
-  min?: number;
-  max?: number;
   value?: number;
   onChange: (value: number) => void;
 }
 
-const parse = (
-  value: string,
-  { min, max }: Pick<NumberInputProps, 'step' | 'min' | 'max'>,
-): number | undefined => {
+const parse = (value: string): number | undefined => {
+  if (value === '') {
+    return 0;
+  }
   const numVal = parseFloat(value.replace(',', '.'));
   if (!isNaN(numVal)) {
-    if (min !== undefined && numVal < min) {
-      return undefined;
-    }
-    if (max !== undefined && numVal > max) {
-      return undefined;
-    }
     return numVal;
   }
   return undefined;
 };
 
-const format = (value: number | undefined): string => String(value ?? '');
+const format = (value: number | undefined): string => String(value ?? '').replace('.', ',');
 
 const numRegex = /^\d*[.,]?\d*$/;
 
-export const NumberInput: FC<NumberInputProps> = ({
-  value,
-  onChange,
-  step,
-  min,
-  max,
-  ...inputProps
-}) => {
+export const NumberInput: FC<NumberInputProps> = ({ value, onChange, ...inputProps }) => {
   const [textValue, setTextValue] = useState(format(value));
+
+  useEffect(() => {
+    if (value !== parse(textValue)) {
+      setTextValue(format(value));
+    }
+  }, [textValue, value]);
 
   const onInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     if (!numRegex.test(event.target.value)) {
       return false;
     }
-    const newVal = parse(event.target.value, { step, min, max });
+    const newVal = parse(event.target.value);
     if (newVal) {
       if (newVal !== value) {
         onChange(newVal);
