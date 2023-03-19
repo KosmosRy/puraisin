64711c3b38f679ff74c18e5fdd94c9d7ed33dee2
Teppo Jalava <tjjalava@iki.fi>
2023-03-20 01:01:35 +0200

    use ssl with db

--- src/utils/certificates.ts
+++ src/utils/certificates.ts
@@ -1,6 +1,5 @@
-import type pg from 'pg-promise/typescript/pg-subset';
-
-const SUPABASE_CERT = `
+export const certificates = {
+  'db.jwokcplmmeskslmqzssv.supabase.co': `
 -----BEGIN CERTIFICATE-----
 MIIDxDCCAqygAwIBAgIUbLxMod62P2ktCiAkxnKJwtE9VPYwDQYJKoZIhvcNAQEL
 BQAwazELMAkGA1UEBhMCVVMxEDAOBgNVBAgMB0RlbHdhcmUxEzARBgNVBAcMCk5l
@@ -24,49 +23,5 @@ Cea13BX2ZgJc7Au30vihLhub52De4P/4gonKsNHYdbWjg7OWKwNv/zitGDVDB9Y2
 CMTyZKG3XEu5Ghl1LEnI3QmEKsqaCLv12BnVjbkSeZsMnevJPs1Ye6TjjJwdik5P
 o/bKiIz+Fq8=
 -----END CERTIFICATE-----
-`;
-
-export interface SlackConfig {
-  botToken: string;
-  clientId: string;
-  clientSecret: string;
-  channelId: string;
-}
-
-export interface Config {
-  slack: SlackConfig;
-  db: pg.IConnectionParameters;
-}
-
-const parseNumber = (value: string) => {
-  const numVal = parseInt(value, 10);
-  if (isNaN(numVal)) {
-    throw new Error(`Invalid value (${value}) for env variable: number expected`);
-  }
-  return numVal;
+`,
 };
-
-const config: Config = {
-  slack: {
-    botToken: process.env.BOT_TOKEN,
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    channelId: process.env.CHANNEL_ID,
-  },
-  db: {
-    host: process.env.DB_HOST,
-    port: parseNumber(process.env.DB_PORT),
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    ssl:
-      process.env.DB_SSL === 'true'
-        ? {
-            rejectUnauthorized: true,
-            ca: SUPABASE_CERT,
-          }
-        : undefined,
-  },
-} as const;
-
-export default config;
