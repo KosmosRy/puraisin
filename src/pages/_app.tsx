470c9ab872a8b192ed0f3b2fdb2c87b65f33b31a
Teppo Jalava <tjjalava@iki.fi>
2023-03-20 00:27:23 +0200

    add analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/global.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
@@ -11,6 +12,7 @@ export default function App({ Component, pageProps }: AppProps): JSX.Element {
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
       </Head>
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
