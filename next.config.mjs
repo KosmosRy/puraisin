8e63a64947d4f5029550c6a214aa8c90f65d88d9
Teppo Jalava <tjjalava@iki.fi>
2024-03-07 19:19:18 +0200

    depsut ajan tasalle

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,5 @@
-const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin');
+import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin';
+
 const withVanillaExtract = createVanillaExtractPlugin();
 
 /** @type {import('next').NextConfig} */
@@ -30,4 +31,4 @@ const nextConfig = {
   },
 };
 
-module.exports = withVanillaExtract(nextConfig);
+export default withVanillaExtract(nextConfig);
